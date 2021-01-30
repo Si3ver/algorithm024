fa06ddde15d55da155a23fe86d427e221cb54810
alwynzhou <alwynzhou@webank.com>
2021-01-30 14:56:01 +0800

    add week1 homework

---
+++ Week_01/homework.js
@@ -0,0 +1,113 @@
+/**
+ * 1. 用 add first 或 add last 这套新的 API 改写 Deque 的代码
+ * 思路： ？？
+ */
+
+
+/**
+ * 2. 分析 Queue 和 Priority Queue 的源码
+ * 思路： ？？
+ */
+
+/**
+ * 3. 删除排序数组中的重复项（Facebook、字节跳动、微软在半年内面试中考过）
+ * easy | leetcode-026 | array
+ * https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array/
+ * 思路：
+ */
+
+/**
+ * 4. 旋转数组（微软、亚马逊、PayPal 在半年内面试中考过）
+ * medium | leetcode-189 | array
+ * https://leetcode-cn.com/problems/rotate-array/
+ * 思路：
+ */
+
+/**
+ * 5. 合并两个有序链表（亚马逊、字节跳动在半年内面试常考）
+ * leetcode-021 | 
+ * https://leetcode-cn.com/problems/merge-two-sorted-lists/
+ */
+var mergeTwoLists = function(l1, l2) {
+
+};
+
+/**
+ * 6. 合并两个有序数组（Facebook 在半年内面试常考）
+ * leetcode-088 |
+ * https://leetcode-cn.com/problems/merge-sorted-array/
+ */
+var merge = function(nums1, m, nums2, n) {
+
+};
+
+
+/**
+ * 7. 两数之和（亚马逊、字节跳动、谷歌、Facebook、苹果、微软在半年内面试中高频常考）
+ * leetcode-001
+ * https://leetcode-cn.com/problems/two-sum/
+ */
+var twoSum = function (nums, target) {
+
+};
+
+/**
+ * 8. 移动零（Facebook、亚马逊、苹果在半年内面试中考过）
+ * leetcode-283 |
+ * https://leetcode-cn.com/problems/move-zeroes/
+ */
+var moveZeroes = function(nums) {
+
+};
+
+/**
+ * 9. 加一（谷歌、字节跳动、Facebook 在半年内面试中考过）
+ * leetcode-066 |
+ * https://leetcode-cn.com/problems/plus-one/
+ */
+var plusOne = function(digits) {
+
+};
+
+/**
+ * 10. 设计循环双端队列（Facebook 在 1 年内面试中考过）
+ * leetcode-641 |
+ * https://leetcode-cn.com/problems/design-circular-deque/?utm_source=LCUS&utm_medium=ip_redirect&utm_campaign=transfer2china
+ */
+var MyCircularDeque = function(k) {
+
+};
+MyCircularDeque.prototype.insertFront = function(value) {
+
+};
+MyCircularDeque.prototype.insertLast = function(value) {
+
+};
+MyCircularDeque.prototype.deleteFront = function() {
+
+};
+MyCircularDeque.prototype.deleteLast = function() {
+
+};
+MyCircularDeque.prototype.getFront = function() {
+
+};
+MyCircularDeque.prototype.getRear = function() {
+
+};
+MyCircularDeque.prototype.isEmpty = function() {
+
+};
+MyCircularDeque.prototype.isFull = function() {
+
+};
+
+
+/**
+ * 11. 接雨水（亚马逊、字节跳动、高盛集团、Facebook 在半年内面试常考）
+ * leetcode-042 |
+ * https://leetcode.com/problems/trapping-rain-water/
+ */
+var trap = function(height) {
+    
+};
