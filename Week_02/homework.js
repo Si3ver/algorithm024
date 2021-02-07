0eca4fd317aa78dfa51f46f97ef26f4373256871
Si3ver <izhouwl@163.com>
2021-02-08 00:54:28 +0800

    complete homework

--- Week_02/homework.js
+++ Week_02/homework.js
@@ -103,15 +103,6 @@ var preorder = function (root) {
  * 6. 字母异位词分组（亚马逊在半年内面试中常考）
  * https://leetcode-cn.com/problems/group-anagrams/
  * medium | leetcode 049
- */
-/**
- * @param {string[]} strs
- * @return {string[][]}
- * 思路：记录到哈希表中，key值为统计数组toString，value值为字符串数组。最后返回 map 所有的values
- */
-/**
- * @param {string[]} strs
- * @return {string[][]}
  * 思路：记录到哈希表中，key值为统计数组toString，value值为字符串数组。最后返回 map 所有的values
  */
 var groupAnagrams = function(strs) {
@@ -191,10 +182,47 @@ var postorder = function (root) {
 /**
  * 10. 丑数（字节跳动在半年内面试中考过）
  * https://leetcode-cn.com/problems/ugly-number/
- * medium | leetcode-
+ * easy | leetcode-263
+ * 思路： 两重循环，一直判断是否整除2、3、5无余数。最后判断是否得到1
  */
+var isUgly = function(num) {
+  for(var p of [2, 3, 5]) {
+    while(num && num % p === 0) {
+      num /= p
+    }
+  }
+  return num === 1
+};
 
 /**
  * 11. 前 K 个高频元素（亚马逊在半年内面试中常考）
- *
+ * https://leetcode-cn.com/problems/top-k-frequent-elements/
+ * medium ｜ leetcode-347
+ * 思路1:
+ * Step1: map统计 O(n)
+ * Step2: 放入数组
+ * Step3: 按照 v 排序 O(nlogn)
+ * Step4: 取出前k个
  */
+var topKFrequent = function(nums, k) {
+  var map = {}
+  nums.forEach(num => {
+    if (map[num] === void(0)) {
+      map[num] = 1
+    } else {
+      ++map[num]
+    }
+  })
+  var arr = []
+  for(var key in map) {
+    arr.push([key, map[key]])
+  }
+  arr.sort(function(a, b) {
+    return b[1] - a[1]
+  })
+  var res = []
+  for(var i = 0; i < k; ++i) {
+    res.push(arr[i][0])
+  }
+  return res
+};
