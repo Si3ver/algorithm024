9c600d8fb4ca42fba623f4f398eec066fc337b8e
Si3ver <izhouwl@163.com>
2021-02-20 12:38:54 +0800

    solve subsets

--- Week_03/078subsets.js
+++ Week_03/078subsets.js
@@ -1,35 +1,57 @@
 /**
  * https://leetcode-cn.com/problems/subsets/
- * 思路：
+ * 思路1：
  * 分治，求子问题。每一步有两种情况，选or不选
  * 
  * @param {number[]} nums
  * @return {number[][]}
  */
 const subsets = function(nums) {
-  if (Object.prototype.toString.call(nums) !== '[object Array]' || nums.length < 1) return []
+  if (Object.prototype.toString.call(nums) !== '[object Array]' || nums.length < 1) return [[]]
   const res = []
 
-  const helper = (curIdx, list) => {
-    if (curIdx === nums.length) {
+  const dfs = (index, list) => {1
+    // terminator
+    if (index === nums.length) {
       res.push(list.slice())
       return
     }
-    helper(curIdx + 1, list)
-    const listClone = list.slice()
-    listClone.push(nums[curIdx])
-    helper(curIdx + 1, listClone)
+    // process1: not pick
+    dfs(index + 1, list)
+    // process2: pick
+    list.push(nums[index])
+    dfs(index + 1, list)
+    // revert states
+    list.pop()
   }
-  helper(0, [])
+
+  dfs(0, [])
   return res
 }
 
 
+// 思路2: 迭代
+const subsets2 = function(nums) {
+  if (Object.prototype.toString.call(nums) !== '[object Array]' || nums.length < 1) return [[]]
+
+  const res = [[]]
+  nums.forEach(num => {
+    res.forEach(r => {
+      res.push(r.concat(num))
+    })
+  })
+  return res
+}
+
+
+
 // ---- test case ---
-console.log(subsets([1]))
-console.log(subsets([1,2]))
-console.log(subsets([1,2,3]))
-console.log(subsets([1,2,3,4]))
+// console.log(subsets([1]))
+// console.log(subsets([1,2]))
+// console.log(subsets([1,2,3]))
+// console.log(subsets([1,2,3,4]))
+console.log(subsets2([1,2,3]))
+console.log(JSON.stringify(subsets2([1,2,3])))
 
 
 /*
