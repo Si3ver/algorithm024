5b2681ac4c4aef69bb110acce4b7809bceeaee05
Si3ver <izhouwl@163.com>
2021-02-27 23:31:37 +0800

    solve 022 with recur & iter

--- Week_03/022generateParenthesis.js
+++ Week_03/022generateParenthesis.js
@@ -1,39 +1,25 @@
 /**
  * medium
  * https://leetcode-cn.com/problems/generate-parentheses/
- * @param {number} n
- * @return {string[]}
+ * 
  * 思路：递归、辅助函数、剪枝
  */
+
+// 解法一：递归
 const generateParenthesis = function(n) {
-  /**
-   * 辅助函数
-   * @param {Number} left 当前左括号的个数
-   * @param {Number} right 当前右括号的个数
-   * @param {Number} n 括号配额
-   * @param {String} s 当前子问题的生成结果
-   */
-  const _generate = function(left, right, n, s) {
-    // terminator
-    if (left === n && right === n) {
-      res.push(s)
-      return null
+  const _generate = (cntL, cntR, curStr) => {
+    if (cntL === n && cntR === n) {
+      res.push(curStr)
+      return
     }
-    // process
-    // drill down
-    if (left < n) {
-      _generate(left + 1, right, n, s + '(')
-    }
-    if (right < left) {
-      _generate(left, right + 1, n, s + ')')
-    }
-    // reverse states
+    if (cntL < n)    _generate(cntL + 1, cntR, curStr + '(')
+    if (cntR < cntL) _generate(cntL, cntR + 1, curStr + ')')
   }
 
   const res = []
-  _generate(0, 0, n, '')
+  _generate(0, 0, '')
   return res
-};
+}
 
 // ---- test case ----
 console.log(generateParenthesis(3))
--- Week_04/022generateParenthesis.js
+++ Week_04/022generateParenthesis.js
@@ -1,7 +1,37 @@
 /**
  * https://leetcode-cn.com/problems/generate-parentheses/
- * 
+ * 22. 括号生成
+ * medium
  */
-const generateParenthesis = function(n) {
 
+// 解法一：递归 + 剪枝
+const generateParenthesis1 = function(n) {
+  const _generate = (cntL, cntR, curStr) => {
+    if (cntL === n && cntR === n) {
+      res.push(curStr)
+      return
+    }
+    if (cntL < n)    _generate(cntL + 1, cntR, curStr + '(')
+    if (cntR < cntL) _generate(cntL, cntR + 1, curStr + ')')
+  }
+
+  const res = []
+  _generate(0, 0, '')
+  return res
 }
+
+// 解法二：非递归（自己维护栈） + 剪枝
+const generateParenthesis2 = function(n) {
+  const res = [], stack = [['(', 1, 0]]
+  while (stack.length) {
+    const [curStr, cntL, cntR] = stack.pop()
+    if (cntL === n && cntR === n) res.push(curStr)
+    if (cntR < cntL) stack.push([curStr + ')', cntL, cntR + 1])
+    if (cntL < n)    stack.push([curStr + '(', cntL + 1, cntR])
+  }
+  return res
+}
+
+
+console.log(generateParenthesis1(3))
+console.log(generateParenthesis2(3))
