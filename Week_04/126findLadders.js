b1a0b524484c00fad37d744ec141169e586a5e9b
Si3ver <izhouwl@163.com>
2021-03-01 00:00:29 +0800

    word ladder

--- Week_04/126findLadders.js
+++ Week_04/126findLadders.js
@@ -1,12 +1,44 @@
 /**
- * https://leetcode-cn.com/problems/word-ladder-ii/description/
+ * https://leetcode-cn.com/problems/word-ladder-ii
  * hard
+ * 126. 单词接龙 II
  * 
  * @param {string} beginWord
  * @param {string} endWord
  * @param {string[]} wordList
  * @return {string[][]}
  */
-const findLadders = function(beginWord, endWord, wordList) {
 
+// BFS
+const findLadders = function(beginWord, endWord, wordList) {
+  const res = [],
+        queue = [[1, [beginWord]]],
+        wordsSet = new Set(wordList),
+        alphabets = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  while (queue.length) {
+    // console.log("🚀", queue)
+    const [level, path] = queue.pop(),
+          word = path[path.length - 1]
+    
+    if (word === endWord) {
+      res.push(path.slice())
+    } else {
+      for (let i = 0; i < word.length; ++i) {
+        for (const ch of alphabets) {
+          const nextWord = word.slice(0, i) + ch + word.slice(i + 1)
+          if (wordsSet.has(nextWord)) {
+            wordsSet.delete(nextWord)
+            queue.unshift([level + 1, path.concat([nextWord])])
+          }
+        }
+
+      }
+
+    }
+  }
+  return res
 }
+
+// ---- test case ----
+console.log(findLadders("hit", "cog", ["hot","dot","dog","lot","log","cog"]))
+// console.log(findLadders("hit", "cog", ["hot","dot","dog","lot","log"]))
--- Week_04/127ladderLength.js
+++ Week_04/127ladderLength.js
@@ -4,6 +4,28 @@
  * 127. 单词接龙
  */
 
+// BFS, 搜索所有可能的单词是否在词库中
+// 复杂度：O(单词个数 * 单词长度 * 26)
 const ladderLength = function(beginWord, endWord, wordList) {
-
+  const wordsSet = new Set(wordList),
+        queue = [[beginWord, 1]],
+        alphbets = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  while (queue.length) {
+    const [word, level] = queue.pop()
+    if (word === endWord) return level
+    for (let i = 0; i < word.length; ++i) {
+      for (const ch of alphbets) {
+        const nextWord = word.slice(0, i) + ch + word.slice(i + 1)
+        if (wordsSet.has(nextWord)) {
+          wordsSet.delete(nextWord)
+          queue.unshift([nextWord, level + 1])
+        }
+      }
+    }
+  }
+  return 0
 }
+
+// ---- test case ----
+console.log(ladderLength('hit', 'cog', ['hot', 'dot', 'dog', 'lot', 'log', 'cog']))
+console.log(ladderLength('hit', 'cog', ['hot', 'dot', 'dog', 'lot', 'log']))
