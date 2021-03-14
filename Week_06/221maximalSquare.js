2fe346be61cf92f8cf3486d1065548c4d60cd70d
Si3ver <izhouwl@163.com>
2021-03-15 01:06:33 +0800

    solve 2 dp problem

--- Week_06/221maximalSquare.js
+++ Week_06/221maximalSquare.js
@@ -2,10 +2,27 @@
  * https://leetcode-cn.com/problems/maximal-square/
  * 221. 最大正方形 | medium
  * 
+ * f(i, j) 表示当前节点可往左上角扩散的正方形边长
+ * f(i, j) = min{ f(i-1, j - 1), f(i, j -1), f(i - 1, j)} + 1
  */
 
 const maximalSquare = function(A) {
+  if (!Array.isArray(A) || A.length < 1 ||
+      !Array.isArray(A[0]) || A[0].length < 1) return 0
 
+  const m = A.length, n = A[0].length
+  const dp = Array(m + 1).fill(0)
+    .map(_ => Array(n + 1).fill(0))
+  let max = 0
+  for (let i = 1; i <= m; ++i) {
+    for (let j = 1; j <= n; ++j) {
+      if (A[i - 1][j - 1] != 0) {
+        dp[i][j] = Math.min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]) + 1
+        max = Math.max(dp[i][j], max)
+      }
+    }
+  }
+  return max * max
 }
 
 // ---- test case ----
--- Week_06/621leastInterval.js
+++ Week_06/621leastInterval.js
@@ -4,11 +4,31 @@
  * 
  */
 
+// 后面的计算方法有点烧脑。。。
 const leastInterval = function(tasks, n) {
-
+  const startCodePoint = 'A'.codePointAt(0)
+  let counter = Array(26).fill(0)
+  let maxCnt = max = 0
+  for (const task of tasks) {
+    const idx = task.codePointAt(0) - startCodePoint
+    counter[idx]++
+    if (counter[idx] == max) maxCnt++
+    else if (counter[idx] > max) {
+      max = counter[idx]
+      maxCnt = 1
+    }
+  }
+  const emptySlots = (max - 1) * (n - (maxCnt - 1))
+  const availTasks = tasks.length - max * maxCnt
+  const idles = Math.max(0, emptySlots - availTasks)
+  return tasks.length + idles;
 }
 
 // ---- test case ----
-console.log(leastInterval(["A","A","A","B","B","B"], 2))
-console.log(leastInterval(["A","A","A","B","B","B"], 0))
-console.log(leastInterval(["A","A","A","A","A","A","B","C","D","E","F","G"], 2))
+console.log(leastInterval(
+  ["A","A","A","B","B","B"], 2))  // A B x A B x A B x
+console.log(leastInterval(
+  ["A","A","A","B","B","B"], 0))
+console.log(leastInterval(
+  ["A","A","A","A","A","A","B","C","D","E","F","G"], 2))
+//  A  B
