8f7736e9a930ec1f7333dd914bd18dfff0335510
Si3ver <izhouwl@163.com>
2021-03-15 00:07:49 +0800

    refactor: transfer trieNode

--- Week_08/212findWords.js
+++ Week_08/212findWords.js
@@ -5,6 +5,47 @@
  * trie树
  */
 
+const findWords = function(A, words) {
+  if (!Array.isArray(words) || words.length < 1 ||
+      !Array.isArray(A) || A.length < 1 ||
+      !Array.isArray(A[0]) || A[0].length < 1) return []
+  // 1. 构建 trie 树
+  const trie = new Trie()
+  for (const word of words) {
+    trie.insert(word)
+  }
+  const dfs = (i, j, prefix, trieNode) => {
+    prefix += A[i][j]
+    trieNode = trieNode[A[i][j]]
+    if (trieNode && trieNode.isWord) res.add(prefix)
+    const tmp = A[i][j]
+    A[i][j] = TAG // 标记已访问过的节点
+    for (let direct = 0; direct <= 3; ++direct) {
+      const x = i + dx[direct]
+      const y = j + dy[direct]
+      if (x >= 0 && x < m &&
+          y >= 0 && y < n &&
+          A[x][y] != TAG &&
+          trieNode[A[x][y]]) {
+        dfs(x, y, prefix, trieNode)
+      }
+    }
+    A[i][j] = tmp // 去除标记
+  }
+  // 2. 启动DFS
+  const res = new Set(), m = A.length, n = A[0].length
+  const TAG = '@'
+  const dx = [1, 0, -1, 0]
+  const dy = [0, 1, 0, -1]
+  for (let i = 0; i < m; ++i) {
+    for (let j = 0; j < n; ++j) {
+      if (trie.startsWith(A[i][j])) {
+        dfs(i, j, '', trie.root)
+      }
+    }
+  }
+  return [...res]
+}
 class Trie {
   constructor() {
     this.root = {}
@@ -37,48 +78,6 @@ class Trie {
     const node = this._traverse(prefix)
     return !!node
   }
-}
-
-const findWords = function(A, words) {
-  if (!Array.isArray(words) || words.length < 1 ||
-      !Array.isArray(A) || A.length < 1 ||
-      !Array.isArray(A[0]) || A[0].length < 1) return []
-  // 1. 构建 trie 树
-  const trie = new Trie()
-  for (const word of words) {
-    trie.insert(word)
-  }
-  
-  const dfs = (i, j, prefix, trie) => {
-    prefix += A[i][j]
-    if (trie.search(prefix)) res.add(prefix)
-    const tmp = A[i][j]
-    A[i][j] = TAG // 标记已访问过的节点
-    for (let direct = 0; direct <= 3; ++direct) {
-      const x = i + dx[direct]
-      const y = j + dy[direct]
-      if (x >= 0 && x < m &&
-          y >= 0 && y < n &&
-          A[x][y] != TAG &&
-          trie.startsWith(prefix)) {
-        dfs(x, y, prefix, trie)
-      }
-    }
-    A[i][j] = tmp // 去除标记
-  }
-  // 2. 启动DFS
-  const res = new Set(), m = A.length, n = A[0].length
-  const TAG = '@'
-  const dx = [1, 0, -1, 0]
-  const dy = [0, 1, 0, -1]
-  for (let i = 0; i < m; ++i) {
-    for (let j = 0; j < n; ++j) {
-      if (trie.startsWith(A[i][j])) {
-        dfs(i, j, '', trie)
-      }
-    }
-  }
-  return [...res]
 }
 
 // ---- test words ----
