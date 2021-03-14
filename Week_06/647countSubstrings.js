b9aeda56a52161122ebd9a37b7a22200ed393e8b
Si3ver <izhouwl@163.com>
2021-03-14 18:11:59 +0800

    solve 647 countSubstrings

--- Week_06/647countSubstrings.js
+++ Week_06/647countSubstrings.js
@@ -5,11 +5,22 @@
  */
 
 const countSubstrings = function(s) {
+  if (!s.length) return 0
+  let cnt = 0, n = s.length
 
+  const help = (l, r) => {
+    while (l >= 0 && r < n && s[l--] === s[r++]) {
+      ++cnt
+    }
+  }
+  
+  for (let i = 0; i < n; ++i) {
+    help(i, i)
+    help(i, i + 1)
+  }
+  return cnt
 }
 
 // ---- test case ----
-console.log(countSubstrings('12'))
-console.log(countSubstrings('226'))
-console.log(countSubstrings('0'))
-console.log(countSubstrings('06'))
+console.log(countSubstrings('abc'))
+console.log(countSubstrings('aaa'))
