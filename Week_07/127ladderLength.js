b87903fbbcdab9b0b3fd5ed3eba693667279858b
Si3ver <izhouwl@163.com>
2021-03-21 16:53:17 +0800

    review BFS

--- Week_07/127ladderLength.js
+++ Week_07/127ladderLength.js
@@ -3,8 +3,35 @@
  * 127. 单词接龙 | hard
  */
 
-const ladderLength = function(beginWord, endWord, wordList) {
+// 解法一：BFS + 回溯 + 剪枝
+const ladderLength1 = function(beginWord, endWord, wordList) {
+  const wordSet = new Set(wordList)
+  const queue = [[beginWord, 1]]
+  const alphas = [...'abcdefghijklmnopqrstuvwxyz']
+  while (queue.length > 0) {
+    const [curWord, level] = queue.pop()
+    if (curWord === endWord) return level
+    for (let i = 0; i < curWord.length; ++i) {
+      for (const ch of alphas) {
+        if (ch === curWord[i]) continue // prun
+        const nextWord = curWord.slice(0, i) + ch + curWord.slice(i + 1)
+        if (wordSet.has(nextWord)) {
+          wordSet.delete(nextWord)
+          queue.unshift([nextWord, level + 1])
+        }
+      }
+    }
+  }
+  return 0
+}
 
+// 解法二：双向BFS
+const ladderLength = function(beginWord, endWord, wordList) {
+  // TODO 
 }
 
 // ---- test case ----
+console.log(ladderLength('abc', 'def', ['dbc', 'bbc', 'dec', 'def', 'log', 'cog'])) // 4
+console.log(ladderLength('hit', 'cog', ['hot', 'dot', 'dog', 'lot', 'log', 'cog'])) // 5
+console.log(ladderLength('hit', 'cog', ['hot', 'dot', 'dog', 'lot', 'log']))  // 0
+console.log(ladderLength("qa","sq",["si","go","se","cm","so","ph","mt","db","mb","sb","kr","ln","tm","le","av","sm","ar","ci","ca","br","ti","ba","to","ra","fa","yo","ow","sn","ya","cr","po","fe","ho","ma","re","or","rn","au","ur","rh","sr","tc","lt","lo","as","fr","nb","yb","if","pb","ge","th","pm","rb","sh","co","ga","li","ha","hz","no","bi","di","hi","qa","pi","os","uh","wm","an","me","mo","na","la","st","er","sc","ne","mn","mi","am","ex","pt","io","be","fm","ta","tb","ni","mr","pa","he","lr","sq","ye"])) // 5
--- Week_07/433minMutation.js
+++ Week_07/433minMutation.js
@@ -4,9 +4,34 @@
  * 
  */
 
-// 
+// 解法一：BFS + 回溯 + 剪枝
+const minMutation1 = function(start, end, bank) {
+  const bankSet = new Set(bank)
+  const genes = [...'ACGT']
+  const queue = [[start, 0]]
+  while (queue.length > 0) {
+    const [curStr, level] = queue.pop()
+    if (curStr === end) return level
+    for (let i = 0; i < curStr.length; ++i) {
+      for (const ch of genes) {
+        if (ch === curStr[i]) continue
+        const nextStr = curStr.slice(0, i) + ch + curStr.slice(i + 1)
+        if (bankSet.has(nextStr)) {
+          bankSet.delete(nextStr)
+          queue.unshift([nextStr, level + 1])
+        }
+      }
+    }
+  }
+  return -1
+}
+
+// 解法二：双向BFS
 const minMutation = function(start, end, bank) {
-  
+  // TODO 
 }
 
 // ---- test case ----
+console.log(minMutation('AACCGGTT', 'AACCGGTA', ['AACCGGTA']))  // 1
+console.log(minMutation('AACCGGTT', 'AAACGGTA', ['AACCGGTA', 'AACCGCTA', 'AAACGGTA'])) // 2
+console.log(minMutation('AAAAACCC', 'AACCCCCC', ['AAAACCCC', 'AAACCCCC', 'AACCCCCC']))  // 3
