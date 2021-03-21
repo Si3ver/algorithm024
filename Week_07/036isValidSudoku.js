d343923c61b608b159ce984c75c7eaf97fe2bce0
Si3ver <izhouwl@163.com>
2021-03-21 16:16:01 +0800

    sudoku

--- Week_07/036isValidSudoku.js
+++ Week_07/036isValidSudoku.js
@@ -3,8 +3,44 @@
  * 36. 有效的数独 | medium
  */
 
-const isValidSudoku = function(board) {
+// O(1)
+const isValidSudoku = function(A) {
+  for (let i = 0; i < 9; ++i) {
+    const row = new Set()
+    const col = new Set()
+    const box = new Set()
 
+    for (let j = 0; j < 9; ++j) {
+      const rItem = A[i][j]
+      const cItem = A[j][i]
+      const bItem = A[3 * Math.floor(i / 3) + Math.floor(j / 3)][3 * (i % 3) + (j % 3)]
+      
+      if (rItem !== '.') {
+        if (row.has(rItem)) return false
+        row.add(rItem)
+      }
+      if (cItem !== '.') {
+        if (col.has(cItem)) return false
+        col.add(cItem)
+      }
+      if (bItem !== '.') {
+        if (box.has(bItem)) return false
+        box.add(bItem)
+      }
+    }
+  }
+  return true
 }
 
 // ---- test case ----
+console.log(isValidSudoku([
+  ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+  ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+  [".", "9", "8", ".", ".", ".", ".", "6", "."],
+  ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+  ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+  ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+  [".", "6", ".", ".", ".", ".", "2", "8", "."],
+  [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+  [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+])) // true
--- Week_07/037solveSudoku.js
+++ Week_07/037solveSudoku.js
@@ -3,8 +3,77 @@
  * 37. 解数独 | hard
  */
 
-const solveSudoku = function(board) {
+// set 交集
+function intersection(set1, set2) {
+  return new Set([...set1].filter(x => set2.has(x)))
+}
 
+function solveSudoku(A) {
+  const init = (n) => 
+    Array(n).fill(0)
+      .map(
+        _ => 
+          new Set(
+            Array(n).fill(0)
+              .map(
+                (_, i) => i + 1
+              )
+          )
+      )
+  const row = init(9)   // 行剩余可用数字
+  const col = init(9)   // 列剩余可用数字
+  const box = init(9)   // 块剩余可用数字
+  const empty = []      // 收集需填数位置
+  for (let i = 0; i < 9; ++i) {
+    for (let j = 0; j < 9; ++j) {
+      if (A[i][j] === '.') {
+        empty.push([i, j])
+      } else {
+        const val = Number(A[i][j])
+        row[i].delete(val)
+        col[j].delete(val)
+        box[Math.floor(i / 3) * 3 + Math.floor(j / 3)].delete(val)
+      }
+    }
+  }
+  // console.log(row, col, box, empty)
+  backtrack(A, row, col, box, empty, 0)
+}
+
+// 对于每一个 empty 坐标，试探所有 set 交集中的数值，没有路了就回溯
+function backtrack(A, row, col, box, empty, iter) {
+  if (iter === empty.length) return true
+  const [i, j] = empty[iter]
+  const b = Math.floor(i / 3) * 3 + Math.floor(j / 3)
+  const validSet = intersection(intersection(row[i], col[j]), box[b])
+  // console.log("🚀validSet", i, j, validSet)
+  for (const val of validSet) {
+    // console.log("🚀", iter, ': ', i, j, val)
+    row[i].delete(val)
+    col[j].delete(val)
+    box[b].delete(val)
+    A[i][j] = String(val)
+    if (backtrack(A, row, col, box, empty, iter + 1)) {
+      return true
+    }
+    row[i].add(val)
+    col[j].add(val)
+    box[b].add(val)
+  }
+  return false
 }
 
 // ---- test case ----
+const board = [
+  ["5", "3", ".", ".", "7", ".", ".", ".", "."],
+  ["6", ".", ".", "1", "9", "5", ".", ".", "."],
+  [".", "9", "8", ".", ".", ".", ".", "6", "."],
+  ["8", ".", ".", ".", "6", ".", ".", ".", "3"],
+  ["4", ".", ".", "8", ".", "3", ".", ".", "1"],
+  ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+  [".", "6", ".", ".", ".", ".", "2", "8", "."],
+  [".", ".", ".", "4", "1", "9", ".", ".", "5"],
+  [".", ".", ".", ".", "8", ".", ".", "7", "9"],
+]
+solveSudoku(board)
+console.log(board)
