987a929485c0c3f547cc9bc96d4e0fb259faa1c8
Si3ver <izhouwl@163.com>
2021-03-22 17:58:54 +0800

    solve merge

--- Week_01/088merge.js
+++ Week_01/088merge.js
@@ -5,18 +5,11 @@
  */
 
 const merge = function(nums1, m, nums2, n) {
-  const toS = Object.prototype.toString, ARRTYPE = '[object Array]'
-  if (toS.call(nums1) !== ARRTYPE || toS.call(nums2) !== ARRTYPE) return
-  
+  if (!Array.isArray(nums1) || !Array.isArray(nums2)) return
+
   let i = m - 1, j = n - 1, k = m + n - 1
-  while(i >= 0 && j >= 0) {
-    if (nums1[i] > nums2[j]) {
-      nums1[k--] = nums1[i--]
-    } else {
-      nums1[k--] = nums2[j--]
-    }
+  while (i >= 0 && j >= 0) {
+    nums1[k--] = nums1[i] > nums2[j] ? nums1[i--] : nums2[j--]
   }
-  while(j >= 0) {
-    nums1[k--] = nums2[j--]
-  }
+  while (j >= 0) nums1[k--] = nums2[j--]
 }
