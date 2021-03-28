9973523406bd4dd47640468ed2195e6e3fe2fd03
Si3ver <izhouwl@163.com>
2021-03-29 00:06:09 +0800

    solve N queens with bitmask

--- Week_08/051solveNQueens.js
+++ Week_08/051solveNQueens.js
@@ -4,8 +4,57 @@
  * 
  */
 
-function solveNQueens(n) {
+function buildResult (res, n) {
+  const m = new Map()
+  const graphes = []
+  res.forEach(state => {
+    const graph = []
+    state.forEach(bit => {
+      let lineStr = ''
+      for (let i = 0; i < n; ++i) {
+        const lastBit = bit & 1
+        bit = bit >> 1
+        lineStr = (lastBit ? 'Q' : '.') + lineStr
+      }
+      graph.push(lineStr)
+    })
+    graphes.push(graph)
+  })
+  return graphes
+}
 
+function solveNQueens(n) {
+  if (n < 1) return []
+
+  /**
+   * DFS
+   * @param {number} row 当前遍历到第几行
+   * @param {number} col 不能用的列
+   * @param {number} pie 不能用的撇
+   * @param {number} na  不能用的捺
+   * 副作用：修改 res
+   */
+  const dfs = (row, col, pie, na, state) => {
+    if (row >= n) {
+      res.push(state)
+      return
+    }
+    let bits = (~(col | pie | na)) & ((1 << n) - 1) // 取得所有得空位
+    while (bits) {
+      const pos = bits & (-bits) // 最低可用位
+      // console.log("🚀",
+      //   bits.toString(2).padStart(4, '0'),
+      //   pos.toString(2).padStart(4, '0')
+      // )
+      bits = bits & (bits - 1)
+      dfs(row + 1, col | pos, (pie | pos) << 1, (na | pos) >> 1, state.concat([pos]))
+    }
+  }
+
+  let res = []
+  dfs(0, 0, 0, 0, [])
+  // console.log("🚀🚀🚀", res)
+  return buildResult(res, n)
 }
 
 // ---- test case ----
--- Week_08/052totalNQueens.js
+++ Week_08/052totalNQueens.js
@@ -4,9 +4,39 @@
  * 
  */
 
+// 位运算
 function totalNQueens(n) {
   if (n < 1) return 0
 
+  /**
+   * DFS
+   * @param {number} row 当前遍历到第几行
+   * @param {number} col 不能用的列
+   * @param {number} pie 不能用的撇
+   * @param {number} na  不能用的捺
+   * 副作用：修改 cnt 值
+   */
+  const dfs = (row, col, pie, na) => {
+    if (row >= n) {
+      ++cnt
+      console.log("🚀🚀🚀", col, pie, na)
+      return
+    }
+    let bits = (~(col | pie | na)) & ((1 << n) - 1) // 取得所有得空位
+    while (bits) {
+      const pos = bits & (-bits) // 最低可用位
+      console.log("🚀",
+        bits.toString(2).padStart(4, '0'),
+        pos.toString(2).padStart(4, '0')
+      )
+      bits = bits & (bits - 1)
+      dfs(row + 1, col | pos, (pie | pos) << 1, (na | pos) >> 1)
+    }
+  }
+
+  let cnt = 0
+  dfs(0, 0, 0, 0)
+  return cnt
 }
 
 // ---- test case ----
