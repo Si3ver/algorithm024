b4a1bc88c27142900583942435d117aa18629da6
Si3ver <izhouwl@163.com>
2021-04-11 00:01:09 +0800

    lcsq

--- Week_10 毕业总结/1143longestCommonSubsequence.js
+++ Week_10 毕业总结/1143longestCommonSubsequence.js
@@ -3,8 +3,23 @@
  * 1143. 最长公共子序列 | medium
  */
 
-function longestCommonSubsequence(s1, s2) {
-
+// 因为要用到dp[i-1][j-1]的值，所以dp开二维数组
+const longestCommonSubsequence = function(s1, s2) {
+  if (typeof s1 !== 'string' || typeof s2 !== 'string' || !s1.length || !s2.length) return 0
+  const m = s1.length, n = s2.length
+  const dp = Array(m + 1).fill(0).map(
+        _ => Array(n + 1).fill(0))
+  
+  for (let i = 1; i <= m; ++i) {
+    for (let j = 1; j <= n; ++j) {
+      if (s1[i - 1] === s2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1
+      } else {
+        dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1])
+      }
+    }
+  }
+  return dp[m][n]
 }
 
 // ---- test case ----
