533376a33f1cc595cf31720c678c2cd5b3db8ddd
Si3ver <izhouwl@163.com>
2023-12-10 00:23:57 +0800

    feat: solve

--- Week_01/linklist/021mergeTwoLists.js
+++ Week_01/linklist/021mergeTwoLists.js
@@ -3,24 +3,24 @@
  * easy | leetcode-021 | link-list
  */
 
-/** 
+/**
  * 21. 合并两个有序链表
  * 思路： 类似归并排序的归并过程
  * 思路： 声明一个空指针头，遍历l1 和 l2，谁的值比较小就把谁拼接在后面
  */
-function mergeTwoLists(l1, l2) {
-  const dummy = new ListNode(-1)
-  let p = dummy
-  while (l1 && l2) {
-    if (l1.val <= l2.val) {
-      p.next = l1
-      l1 = l1.next
-    } else {
-      p.next = l2
-      l2 = l2.next
+var mergeTwoLists = function(l1, l2) {
+    const dummyHead = new ListNode(-1);
+    let p = dummyHead;
+    while (l1 && l2) {
+        if (l1.val <= l2.val) {
+            p.next = l1;
+            l1 = l1.next;
+        } else {
+            p.next = l2;
+            l2 = l2.next;
+        }
+        p = p.next;
     }
-    p = p.next
-  }
-  p.next = l1 || l2
-  return dummy.next
-}
+    p.next = l1 || l2;
+    return dummyHead.next;
+};
