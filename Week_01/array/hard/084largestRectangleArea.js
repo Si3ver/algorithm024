fc865b2c0ba1812f0812a0baffae510cc24585b2
Si3ver <izhouwl@163.com>
2023-12-12 23:38:21 +0800

    feat: 单调栈

--- Week_01/array/hard/084largestRectangleArea.js
+++ Week_01/array/hard/084largestRectangleArea.js
@@ -1,22 +1,28 @@
 /**
  * https://leetcode-cn.com/problems/largest-rectangle-in-histogram/
  * 84. 柱状图中最大的矩形 | hard
+ *
+ * 思路：单调栈存idx，直到遇到右边界才出栈
  */
 
 // stack O(n)
-function largestRectangleArea(A) {
-  let maxArea = 0
-  const stack = []
-  A = [0, ...A, 0]
-  for (let i = 0; i < A.length; ++i) {
-    while (stack.length > 1 && A[stack[stack.length - 1]] > A[i]) {
-      const j = stack.pop()
-      maxArea = Math.max((i - stack[stack.length - 1] - 1) * A[j], maxArea)
+var largestRectangleArea = function(heights) {
+    let max = 0;
+    const stack = [-1];
+    const A = [...heights, 0];
+    for (let i = 0; i < A.length; ++i) {
+        while (stack.length > 1 && A[stack[stack.length - 1]] > A[i]) {
+            const curIdx = stack.pop();
+            const left = stack[stack.length - 1] + 1;
+            const right = i - 1;
+            const curArea = A[curIdx] * (right - left + 1);
+            max = Math.max(max, curArea);
+        }
+        stack.push(i);
     }
-    stack.push(i)
-  }
-  return maxArea
-}
+    return max;
+};
 
 // ---- test case ----
 console.log(largestRectangleArea([2,1,5,6,2,3]))
+console.log(largestRectangleArea([2,4]))
